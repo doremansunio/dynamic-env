486912df9842cfd02a0296fdc8d743358bba7b4c
doremansunio <136595579+doremansunio@users.noreply.github.com>
2024-09-05 09:23:03 +0530

    4

--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "selected_resources" {
-  type = list(object({ name = string, attributes = map(string) }))
-  default = [
-    { name = "project", attributes = {"name": "new-project", "description": "new-project"},
-    { name = "group", attributes = {"name": "new-grp", "description": "new-grp"},
-  ]
-}

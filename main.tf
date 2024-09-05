2c19017bb97e6e38a72d810b55494865dc97835f
doremansunio <136595579+doremansunio@users.noreply.github.com>
2024-09-05 09:36:44 +0530

    p4

--- main.tf
+++ main.tf
@@ -1,9 +1,35 @@
-resource "rafay_project" "tfdemoproject1" {
-  metadata {
-    name        = "tfdemoproject1"
-    description = "terraform project"
-  }
-  spec {
-    default = false
+# variables.tf
+variable "selected_resources" {
+  type = list(object({
+    name = string
+    type = string
+  }))
+}
+
+# main.tf
+resource "null_resource" "trigger_modules" {
+  # This is just a trigger to force module execution
+}
+
+data "template_file" "available_resources" {
+  filename = "resources.json"
+}
+
+variable "resource_config" {
+  type = map(object({
+    # Specific configuration per resource type
+  }))
+}
+
+dynamic "module" {
+  for_each = var.selected_resources
+
+  content {
+    name = each.value.name
+    source = "./modules/${each.value.type}"
+
+    # Pass configuration based on resource type
+    # This might involve accessing resource_config
+    ${each.value.type} = var.resource_config[each.value.type]
   }
 }

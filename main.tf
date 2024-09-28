aa9205c1c2f3d8667440a801d65d7ba60ceafb84
doremansunio <136595579+doremansunio@users.noreply.github.com>
2024-09-28 18:55:22 +0530

    Update main.tf1

--- main.tf
+++ main.tf
@@ -1,34 +1,7 @@
-# variables.tf
-variable "selected_resources" {
-  type = list(object({
-    name = string
-    type = string
-  }))
-}
-
-# main.tf
-resource "null_resource" "trigger_modules" {
-  # This is just a trigger to force module execution
-}
-
-data "template_file" "available_resources" {
-  filename = "resources.json"
-}
-
-variable "resource_config" {
-  type = map(object({
-    # Specific configuration per resource type
-  }))
-}
+# Terraform configuration
 
 
-for_each = var.selected_resources
-
-content {
-  name = each.value.name
-  source = "./modules/${each.value.type}"
-
-  # Pass configuration based on resource type
-  # This might involve accessing resource_config
-  ${each.value.type} = var.resource_config[each.value.type]
+resource "aws_s3_bucket" "s3_buck_1" {
+  region = "ap-south-1"
+  bucket = "test-bucket"
 }
